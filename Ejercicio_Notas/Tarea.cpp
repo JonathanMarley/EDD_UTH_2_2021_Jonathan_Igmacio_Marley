2435f0bc848ecd08184435d1ad4255601792d906
Jonathan Marley <jonathanmarley2536@gmail.com>
2021-05-22 08:51:06 -0600

    Updated

--- Ejercicio_Notas/Tarea.cpp
+++ Ejercicio_Notas/Tarea.cpp
@@ -5,27 +5,27 @@ using namespace std;
 int main(){
 
    string Nombre; 
-   double litros,kilometros;
-   int resultado, millas;
-   int contador,opcion, cantidadLitros, cantidadKilometros;
-   cout<<"---BIENVENIDO AL PROGRANA PARA CALCULAR Litrso y Kilometraje--- "<<endl;
-   cin>>opcion;
-   while (opcion) {
+   double litros=0,kilometros=0;
+   double resultado;
+   double contador,opcion, cantidadLitros, cantidadKilometros;
+   cout<<"---BIENVENIDO AL PROGRANA PARA CALCULAR Litros y Kilometrajes--- "<<endl;
+   while (litros!=1) {
    cout<<"Por favor ingrese su nombre: "<<endl;
    cin>>Nombre;
-   cout<<"Escoja una opcion segun la cantidad de litros 1. = 1 litros, /2. = 3 litros, /3. = 5 litros, /4 = 8 litros en adelante: "<<endl;
+   cout<<"Escoja una opcion segun la cantidad de litros  litros en adelante: "<<endl;
    cin>>cantidadLitros;
    cout<<"Introduzca la cantidad de kilometros: "<<endl;
    cin>>cantidadKilometros;
    cout<<"El nombre de la persona es: "<< Nombre <<endl;
-   if (cantidadLitros == 1 && cantidadKilometros){
-       cantidadKilometros = millas * 1.6093;
-       contador++;
-       cout<<"Los Kilometraje /Litro para este tanque fueron: " << cantidadKilometros <<endl;
+   if (cantidadLitros == 1){
+
    }else{
-       cout<<"No es suficiente"<<endl;
+       resultado = cantidadKilometros / cantidadLitros;
+       cantidadLitros += litros;
+       cantidadKilometros += kilometros;
    }
    
+   cout<<"Los Kilometraje /Litro para este tanque fueron: " << resultado <<endl;
 
    }
    
