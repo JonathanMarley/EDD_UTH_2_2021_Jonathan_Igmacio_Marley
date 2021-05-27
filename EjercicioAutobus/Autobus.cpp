f732e9021f579f219d00b54d689bd53f1bef68ee
Jonathan Marley <jonathanmarley2536@gmail.com>
2021-05-27 17:37:43 -0600

    Updated

--- EjercicioAutobus/Autobus.cpp
+++ EjercicioAutobus/Autobus.cpp
@@ -12,23 +12,38 @@ int main(){
     char resp='s';
 
 
-
     for (int x = 0; x < tamanio; x++){
         AutoBus[x]=0;
     }
     while (resp=='s' && contador<tamanio){
-        cout<<"Digite un numero de asiento: "<<endl;
+
+        cout<<" ****Asiento vacios**** "<<endl;
+        for (int  z = 0; z < tamanio; z++)
+        {
+            if (AutoBus[z]==0)
+            {
+                cout<<"\""<<z+1<<"\" "<<endl;
+            }
+            
+        }
+
+        cout<<" -----Digite un numero de asiento----- "<<endl;
         cin>>NumAsiento;
-        if (AutoBus[NumAsiento-1]==0){
+
+        if (NumAsiento>tamanio)
+        {
+            cout<<" **Asiento no existente ingrese un numero valido** "<<endl;
+        }else
+        {
             AutoBus[NumAsiento-1]=1;
             contador++;
-        }else
-        cout<<"El asiento ya fue reservado"<<endl;
-        
-        cout<<"Quiere continuar vendiendo?"<<endl;
+
+        }
+
+        cout<<" ----Quiere continuar vendiendo?----"<<endl;
         cin>>resp;
-        
-        cout<<"Informes de boletos vendidos y no comprados"<<endl;
+    
+        cout<<" *****Informes de boletos vendidos y no comprados*****"<<endl;
 
         for (size_t y = 0; y < tamanio; y++){
             if (AutoBus[y]==1){
