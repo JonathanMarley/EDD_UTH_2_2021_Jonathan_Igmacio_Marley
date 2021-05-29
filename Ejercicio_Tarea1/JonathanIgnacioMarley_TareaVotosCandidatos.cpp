2f29615527416fdb3ce96c2555f77ab2ad54c4fa
JonathanMarley <jonathanmarley2536@gmail.com>
2021-05-28 23:56:16 -0700

    Updated

--- Ejercicio_Tarea1/JonathanIgnacioMarley_TareaVotosCandidatos.cpp
+++ Ejercicio_Tarea1/JonathanIgnacioMarley_TareaVotosCandidatos.cpp
@@ -5,12 +5,51 @@ using namespace std;
 
 int main(){
     const int tamanio=12;
-    int  candidato[tamanio];
+    int  candidato[tamanio], numeroVoto, i;
+    char resp='s';
 
     for (int m = 0; m < tamanio; m++){
         
         candidato[m]=0;
     }
+    cout<<"__________________________________________________________________________"<<endl;
+    cout<<"             ****BIENVENIDO AL PROGRAMA DE VOTACIONES**** "<<endl;
+    cout<<"__________________________________________________________________________"<<endl;
+    
+    while (resp=='s' && i<tamanio){
+
+        for (int z = 0; z < tamanio; z++){
+        
+        if (candidato[z]==0){
+            
+          cout<< "\" [" <<z+1<< "\"] ";
+        }
+        
+        }
+        cout<<""<<endl;
+        cout<<"__________________________________________________________________________"<<endl;
+        cout<<" ----Ingrese un numero para poder elegir un candidato---- "<<endl;
+        cin>>numeroVoto;
+         cout<<"__________________________________________________________________________"<<endl;
+
+        //Utilizaremos una condicion para indicar que si un numero es mayor le dira que ingrese un numero que sea correcto
+        if (numeroVoto>tamanio){
+            cout<<" **El numero que usted ingreso para poder votar no es valio ingrese un numero correcto** "<<endl;
+        }else{
+            candidato[numeroVoto-1]==1;
+            i++;
+
+        }
+        
+        cout<<" --Quieres seguir votando-- "<<endl;
+        cin>>resp;
+
+        for (int c = 0; c < tamanio; c++){
+            
+        }
+        
+    }
+    
 
 
 
